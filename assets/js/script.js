c1d30baa11c5259da60da824f6d772031dcc7574
Philipe <31636625+PhilipeDev@users.noreply.github.com>
2017-09-08 16:42:21 -0300

    Adicionadas alterações na pasta assets
    Foram adicionadas mais imagens de icones para serem utilizadas na pagina, Adicionado novas classes e comandos CSS, Novo arquivo JavaScript adicionado.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+imagem_Web();
+
+
+function imagem_Web(){
+  document.getElementById('iconeConteudo').src = "assets/img/icon-responsivedesign2.png";
+  setTimeout("imagem_Web2()", 3500);	
+}
+
+function imagem_Web2(){
+	document.getElementById('iconeConteudo').src = "assets/img/icon-html5-js-css3.png";
+	setTimeout("imagem_Web3()", 3500);	
+}
+
+function imagem_Web3(){
+	document.getElementById('iconeConteudo').src = "assets/img/iconBootstrap.png";
+	setTimeout("imagem_Web()", 3500);
+	
+}
