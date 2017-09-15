440e0ac261f323ab7c17a427f9b4470a7a748999
Philipe <31636625+PhilipeDev@users.noreply.github.com>
2017-09-14 21:36:12 -0300

    Alteração em script.js
    Alterada a função que mostrava o icone de responsividade, agora utilizando bootstrap.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,5 @@
 imagem_Web2();
 
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-} 
-
-
 function imagem_Web(){
   document.getElementById('iconeConteudo').src = "assets/img/icon-responsivedesign2.png";
   setTimeout("imagem_Web2()", 3500);	
