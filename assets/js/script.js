cabc04831057446f80035667b9f3f1230b6252bc
Sir Felipe <felipe.dioliveira@outlook.com>
2019-04-19 23:52:05 -0300

    Fixing the media for small devices and adding a little bit of content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,39 @@
 imagem_Web2();
+imagem_back();
 
 function imagem_Web(){
   document.getElementById('iconeConteudo').src = "assets/img/icon-responsivedesign2.png";
-  setTimeout("imagem_Web2()", 3500);	
+  setTimeout("imagem_Web2()", 3500);
 }
 
 function imagem_Web2(){
 	document.getElementById('iconeWeb').src = "assets/img/icon-html5-js-css3.png";
-	setTimeout("imagem_Web3()", 3500);	
+	setTimeout("imagem_Web3()", 3500);
 }
 
 function imagem_Web3(){
 	document.getElementById('iconeWeb').src = "assets/img/iconBootstrap.png";
 	setTimeout("imagem_Web2()", 3500);
-	
+
 }
 
+function imagem_back(){
+  document.getElementById('iconeBack').src = "assets/img/icon-spring-framework.svg";
+  setTimeout("imagem_back2()",3500);
+}
+
+function imagem_back2(){
+  document.getElementById('iconeBack').src = "assets/img/icon_java.png";
+  setTimeout("imagem_back3()",3500);
+}
+
+function imagem_back3(){
+  document.getElementById('iconeBack').src = "assets/img/icon-csharp.png";
+  setTimeout("imagem_back()", 3500);
+}
+
+
+/*
 function imagem_java(){
 	document.getElementById('iconeJava').src = "assets/img/java-icon.png";
 	document.getElementById('iconeJava').className = "java-icon";
@@ -27,3 +45,4 @@ function imagem_java2(){
 	document.getElementById("iconeJava").className = "pythonIcon";
 	setTimeout("imagem_java()", 5000);
 }
+*/
